917168b130d4693f75273ffe8f2c7a55edede09b
Lachlan Underhill <lachlanu@gmail.com>
2020-07-13 20:25:40 +1000

    Fixed spotify module breaking when spotify closed

--- polybar/.config/polybar/scripts/spotify.sh
+++ polybar/.config/polybar/scripts/spotify.sh
@@ -1,15 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$(playerctl --player=playerctld status)" = "Stopped" ]; then
+#
+# Script to display the currently playing song on any player supported
+# by playerctl (usually Spotify)
+#
+
+# If no players are open, show nothing
+if [ "$(playerctl -l | grep -v playerctld | wc -l)" = 0 ]; then
 
   echo ""
 
-elif [ "$(playerctl --player=playerctld status)" = "Paused"  ]; then
-
-  playerctl --player=playerctld metadata --format " {{ title }} - {{ artist }}"
-
-else # Can be configured to output differently when player is paused
+# If music is playing, display a pause icon
+elif [ "$(playerctl status)" = "Playing"  ]; then
 
   playerctl --player=playerctld metadata --format " {{ title }} - {{ artist }}"
+
+# If music is paused, display a play icon
+elif [ "$(playerctl status)" = "Paused"  ]; then
+
+  playerctl --player=playerctld metadata --format " {{ title }} - {{ artist }}"
 
 fi
